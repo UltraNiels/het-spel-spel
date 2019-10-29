0dbe716c4f56fcb0fad499520a1bb92b090afbcc
UltraNiels <niels.huisman@zernike.nl>
2019-10-29 16:41:26 +0100

    state Machine!

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
+let title = 0;
+let state;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  state = 0;
 }
 
 function draw() {
   background(220);
+  if (state == title) titleDraw();
+  if (state == playing) playingDraw();
+}
+
+function playingDraw {
+
+}
+
+function titleDraw {
+	
 }
