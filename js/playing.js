39f206805f359d9ffcf4cca6c9fa92ba4ffd40e4
Niels <45306483+UltraNiels@users.noreply.github.com>
2019-11-02 10:15:40 +0100

    input and it works

--- js/playing.js
+++ js/playing.js
@@ -11,13 +11,17 @@ function newWord() {
 
 mouse_click_actions.push(newWord);
 mouse_click_actions.push(()=>{
-	if (mouseHit(back_icon_rect) && state == playing) state = title;
+	if (state == playing && mouseHit(back_icon_rect)) state = title;
 })
 
 function playingSetup() {
   	wordlist = groep6; words = groep6Woorden; verbs = groep6WerkWoorden;
 	input = createInput();
-  	input.position(w2, h2);
+	input.class('word_input');
+	input.style('width', `${width/3}px`)
+	input.style('height', `${height/8}px`)
+	input.style('font-size', `${min(width / 10, height/ 10)}px`)
+  	input.center();
 	newWord();
   	back_icon_rect = new Rect(sound_icon_rect.w+sound_icon_rect.x+20, sound_icon_rect.y, backIcon.width, backIcon.height);
 }
@@ -27,6 +31,10 @@ function playingDraw() {
 	textSize(min(windowWidth/10, windowHeight/10));
 	text(`${word} (${catname})`, w2, h2/2);
 	image(backIcon, back_icon_rect.x, back_icon_rect.y);
+	if (input.value() == word) {
+		input.value('');
+		newWord();
+	}
 }
 
 function playingEnd() {
